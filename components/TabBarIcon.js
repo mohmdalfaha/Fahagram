c684bfb0a6e19a78411d825ee4e7b3ef2bd79ad8
mohmdalfaha <mohmd1919@gmail.com>
2019-04-03 05:59:25 +0300

    style: Add tabBarIcon gradient focused color

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { Icon } from 'expo';
-import {Constants,LinearGradient} from 'expo';
+import { Icon, Constants,LinearGradient } from 'expo';
+import {StyleSheet} from 'react-native'
 
 import Colors from '../constants/Colors';
 
 export default class TabBarIcon extends React.Component {
   render() {
+    if(this.props.focused){
+      return(
+        <LinearGradient
+                colors = {['rgb(48,35,174)','rgb(200,109,215)']}
+                style = {{width:45,height:45,borderRadius:50}}
+                >
+              <Icon.Ionicons
+                name={this.props.name}
+                size={30}
+                color={Colors.tabIconDefault}
+                style={styles.tabBarIcon}
+
+              />
+            </LinearGradient>
+        )
+    }
     return (
       <Icon.Ionicons
-        name={this.props.name}
-        size={30}
-        color={this.props.focused
-          ? Colors.tabIconSelected
-          : Colors.tabIconDefault}
-      />
+                name={this.props.name}
+                size={30}
+                color={Colors.tabIconDefault}
+                style={styles.tabBarIcon}
+              />
+
     );
   }
 }
 
+const styles = StyleSheet.create({
+  tabBarIcon:{
+    alignSelf:'center',
+    paddingTop:5,
+    paddingRight:2,
+  }
+})
 
